3fd89d836cb6de58e5c67bdf3cc1579c7907b31a
leosquared <leo14358@gmail.com>
2018-01-25 23:10:12 -0500

    capturing whole info box

--- scraper.rb
+++ scraper.rb
@@ -17,17 +17,44 @@ def get_cities
 end
 
 def run_brewery(brewery_link, brewery_name)
+  
   brewery_page = HTTParty.get(brewery_link)
   parse_brewery_page = Nokogiri::HTML(brewery_page)
+  brewery_meta = {}
+  
   ## get various select elements
-  rating = parse_brewery_page.css('div#score_box span[class=ba-ravg]').text.to_f
-  types = parse_brewery_page.css('b:contains("Type:")')[0].next.text.strip
-  address = parse_brewery_page.css('b:contains("Type:")')[0].next.next_element.text.strip
+  brewery_meta["brewery_name"] = brewery_name
+  brewery_meta["rating"] = parse_brewery_page.css('div#score_box span[class=ba-ravg]').text.to_f
+  info_box_text = []
+	
+	parse_brewery_page.css('div#info_box').children.each_with_index do |child, i|
+		if child.text.strip != ""
+			info_box_text << child.text.strip
+		end
+	end
+	
+	addr_elements = []
+	info_box_text.each_with_index do |tag, i|
+		if ["PLACE INFO", "map"].include?(tag)
+		elsif /\([0-9]+\)/.match?(tag)
+			brewery_meta["phone"] = tag
+		elsif /Added by.*/.match?(tag)
+			brewery_meta["author"] = tag
+		elsif /.+(\.com|\.org|\.net)$/.match?(tag)
+			brewery_meta["website"] = tag
+		elsif tag == "Type:"
+			brewery_meta["types"] = info_box_text[i+1].split(", ")
+			info_box_text.delete_at(i+1)
+		elsif tag == "Notes:"
+			brewery_meta["notes"] = info_box_text[i+1]
+			info_box_text.delete_at(i+1)
+		else
+			addr_elements << tag
+		end
+	end
+	brewery_meta["address"] = addr_elements.join(" ")
 
-
-  {"brewery_name" => brewery_name, \
-		"rating" => rating, "types" =>types, \
-	  "address" => address}
+  brewery_meta
 end
 
 def run_city(city, link)
@@ -56,7 +83,7 @@ end
 if __FILE__ == $0
 	@domain_url = 'https://www.beeradvocate.com'
 	write_data = {}
-	city_links = get_cities().to_a[0..1].to_h
+	city_links = get_cities().to_a[0..1].to_h ####### change this to run the whole dataset
 	city_links.each do |city, link|
 		## retry counts for each city
 		retries = 3
